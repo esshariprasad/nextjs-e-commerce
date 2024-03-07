f663ba7df777516a33e749764917f5a1fc470037
sai shiva hari prasad <esshariprasad@gmail.com>
2024-03-06 20:15:29 -0600

    market place

--- app/products/page.js
+++ app/products/page.js
@@ -1,34 +1,28 @@
-"use client"
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default function ProductPage(){
+// app/page.js 
+export default async function ProductPage() {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://65e8a31c4bb72f0a9c4ffc6f.mockapi.io/products');
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return []; // Return an empty array on error
+    }
+  };
 
-    const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://65e8a31c4bb72f0a9c4ffc6f.mockapi.io/products');
-        setProducts(response.data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const products = await fetchData();
 
   return (
     <div>
       <h1>Products</h1>
       <ul>
-        {products.map(product => (
+        {products.map((product) => (
           <li key={product.id}>
             <h2>{product.name}</h2>
             <p>Price: ${product.price}</p>
-            <img src={product.image} alt={product.name}  />
-            {/* No need to display the description */}
+            <img src={product.image} alt={product.name} />
           </li>
         ))}
       </ul>
